06ecc8c079ee7104d2b5c21503d072d0277e1928
Elarsen14 <eml@kvalitetsit.dk>
2022-08-10 15:44:05 +0200

    Updated Exceptions

--- service/src/main/java/dk/kvalitetsit/ihexdsapi/controller/exception/ResourceNotFoundException.java
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/controller/exception/ResourceNotFoundException.java
@@ -1,11 +1,41 @@
 package dk.kvalitetsit.ihexdsapi.controller.exception;
 
+import org.openapitools.model.BasicError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.OffsetDateTime;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
+    public enum ERROR_CODE {
+        GENERIC(1);
+
+        private final int errorCode;
+
+        ERROR_CODE(int errorCode) {
+            this.errorCode = errorCode;
+        }
+    }
+
+    private final BasicError error;
+
+    private ResourceNotFoundException(BasicError error) {
+        this.error = error;
+    }
+
+    public static ResourceNotFoundException createException(String message) {
+        var error = new BasicError();
+        error.setError(message);
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setStatusText(HttpStatus.NOT_FOUND.getReasonPhrase());
+        error.setTimestamp(OffsetDateTime.now());
+        error.setErrorCode(ERROR_CODE.GENERIC.errorCode);
+
+        return new ResourceNotFoundException(error);
+    }
+
+    public BasicError getError() {
+        return error;
     }
 }
