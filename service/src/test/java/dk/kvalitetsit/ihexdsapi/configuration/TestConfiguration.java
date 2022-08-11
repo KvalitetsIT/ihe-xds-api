87f37c98ecfe300803743e402c31484a8ebd1bfd
Elarsen14 <eml@kvalitetsit.dk>
2022-08-11 15:54:23 +0200

    Added a source application.properties

--- service/src/test/java/dk/kvalitetsit/ihexdsapi/configuration/TestConfiguration.java
+++ service/src/test/java/dk/kvalitetsit/ihexdsapi/configuration/TestConfiguration.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 @Import(IheXdsConfiguration.class)
 @EnableAutoConfiguration
+@PropertySource("application.properties")
 public class TestConfiguration extends AbstractTest {
 
     @Autowired
