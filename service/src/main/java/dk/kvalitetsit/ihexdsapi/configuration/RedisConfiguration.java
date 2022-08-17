62926b7ede4a10afc8ee89525aa606e01b8e0ce0
Elarsen14 <eml@kvalitetsit.dk>
2022-08-17 14:54:03 +0200

    Created some redis configuration and set up

---
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/configuration/RedisConfiguration.java
@@ -0,0 +1,47 @@
+package dk.kvalitetsit.ihexdsapi.configuration;
+
+import dk.kvalitetsit.ihexdsapi.dao.impl.CredentialRepositoryImpl;
+import dk.kvalitetsit.ihexdsapi.dgws.CredentialService;
+import dk.kvalitetsit.ihexdsapi.dgws.impl.CredentialServiceImpl;
+import org.apache.bcel.Repository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${redis.host}")
+    private String redisHost;
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory()  {
+        JedisConnectionFactory jedisConFactory
+                = new JedisConnectionFactory();
+        jedisConFactory.setHostName(redisHost);
+        jedisConFactory.setPort(redisPort);
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    public CredentialRepositoryImpl credentialRepository() {
+        return new CredentialRepositoryImpl(redisTemplate());
+    }
+
+
+
+
+
+
+}
