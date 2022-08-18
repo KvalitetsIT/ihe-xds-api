08ef203f7f748083f6eea6fc58dabfc7d05ea90f
Elarsen14 <eml@kvalitetsit.dk>
2022-08-18 15:33:41 +0200

    Clean up

--- service/src/main/java/dk/kvalitetsit/ihexdsapi/dao/CredentialRepository.java
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/dao/CredentialRepository.java
@@ -1,21 +1,16 @@
 package dk.kvalitetsit.ihexdsapi.dao;
 
 import dk.kvalitetsit.ihexdsapi.dao.entity.CredentialInfoEntity;
-import dk.kvalitetsit.ihexdsapi.dgws.CredentialInfo;
 
 import java.util.List;
 
 public interface CredentialRepository {
 
-      boolean saveCredentialsForID(CredentialInfoEntity credentialInfo, int TTL);
+    boolean saveCredentialsForID(CredentialInfoEntity credentialInfo);
 
-      boolean saveListOfCertsForUser(String owner, List<String> list, int TTL);
+    CredentialInfoEntity findCredentialInfoByID(String id);
 
-    CredentialInfoEntity findByID(String id);
-
-    List<String> findByOwner(String owner);
-
-    boolean updateCerts(String owner, List<String> list, int TTL);
+    List<String> FindListOfIDsForOwner(String owner);
 
 
 }
