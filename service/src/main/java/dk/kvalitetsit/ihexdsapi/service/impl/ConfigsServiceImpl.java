678b40b328635574dce12c3b6717034cf3e193eb
Elarsen14 <eml@kvalitetsit.dk>
2022-09-16 12:05:22 +0200

    Added new endpoint to fetch current configs

---
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/service/impl/ConfigsServiceImpl.java
@@ -0,0 +1,62 @@
+package dk.kvalitetsit.ihexdsapi.service.impl;
+
+import dk.kvalitetsit.ihexdsapi.service.ConfigsService;
+import org.openapitools.model.ConfigResponse;
+
+import java.util.*;
+
+public class ConfigsServiceImpl implements ConfigsService {
+
+
+    private String sts, xdsIti18;
+
+    private List<ConfigResponse> configResponseList;
+
+
+    public ConfigsServiceImpl(String sts, String xdsIti18) {
+        this.sts = sts;
+        this.xdsIti18 = xdsIti18;
+
+        makeList();
+
+
+    }
+
+    private void makeList() {
+        List<ConfigResponse> list= new LinkedList<>();
+        // variables
+        list.add(createResponseObject("sts.url", sts));
+        list.add(createResponseObject("iti-18.url", xdsIti18));
+
+        Collections.sort(list, new SortbyKey());
+
+        configResponseList = list;
+
+    }
+
+    private ConfigResponse createResponseObject(String key, String value) {
+
+        ConfigResponse obj = new ConfigResponse();
+        obj.setConfigKey(key);
+        obj.setConfigValue(value);
+
+        return obj;
+    }
+
+
+    @Override
+    public List<ConfigResponse> getListOfConfigResponses() {
+        return this.configResponseList;
+    }
+
+}
+
+class SortbyKey implements Comparator<ConfigResponse> {
+
+
+    @Override
+    public int compare(ConfigResponse o1, ConfigResponse o2) {
+        return o1.getConfigKey().compareToIgnoreCase(o2.getConfigKey());
+    }
+}
+
