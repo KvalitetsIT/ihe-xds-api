891d0af0159a39d5df9e3d394186183dae310ab1
Elarsen14 <eml@kvalitetsit.dk>
2022-10-11 10:22:25 +0200

    cdav-21
    current 10:22

--- service/src/main/java/dk/kvalitetsit/ihexdsapi/service/impl/ConfigsServiceImpl.java
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/service/impl/ConfigsServiceImpl.java
@@ -1,8 +1,6 @@
 package dk.kvalitetsit.ihexdsapi.service.impl;
 
-import dk.kvalitetsit.ihexdsapi.dao.entity.Iti41RepositoryProfile;
 import dk.kvalitetsit.ihexdsapi.service.ConfigsService;
-import dk.kvalitetsit.ihexdsapi.service.Iti41RepositoryProfileService;
 import org.openapitools.model.ConfigResponse;
 
 import javax.annotation.PostConstruct;
@@ -16,15 +14,15 @@ public class ConfigsServiceImpl implements ConfigsService {
 
     private List<ConfigResponse> configResponseList;
 
-    private Iti41RepositoryProfileService iti41RepositoryProfileService;
 
 
-    public ConfigsServiceImpl(String sts, String xdsIti18, String xdsIti43, Iti41RepositoryProfileService iti41RepositoryProfileService) {
+
+    public ConfigsServiceImpl(String sts, String xdsIti18, String xdsIti43) {
         this.sts = sts;
         this.xdsIti18 = xdsIti18;
         this.xdsIti43 = xdsIti43;
 
-        this.iti41RepositoryProfileService = iti41RepositoryProfileService;
+
 
         makeList();
 
@@ -39,7 +37,6 @@ public class ConfigsServiceImpl implements ConfigsService {
         list.add(createResponseObject("iti-18.url", xdsIti18));
         list.add(createResponseObject("iti-43.url", xdsIti43));
 
-        list.add(createResponseObjectForIti41());
 
 
         Collections.sort(list, new SortbyKey());
@@ -59,29 +56,8 @@ public class ConfigsServiceImpl implements ConfigsService {
         return obj;
     }
 
-    private ConfigResponse createResponseObjectForIti41() {
-
-        String str = "";
 
 
-        for (int i = 0; i < this.iti41RepositoryProfileService.getRepositoryProfileList().size(); i++) {
-            Iti41RepositoryProfile repo = this.iti41RepositoryProfileService.getRepositoryProfileList().get(i);
-            String s = repo.getName()  + ": " + repo.getEndpoint();
-
-            if (i + 1 != this.iti41RepositoryProfileService.getRepositoryProfileList().size()) {
-                s = s + "\n";
-            }
-
-            str = str + s;
-        }
-
-
-        ConfigResponse obj = new ConfigResponse();
-        obj.setConfigKey("iti-41.url");
-        obj.setConfigValue(str);
-
-        return obj;
-    }
 
 
     @Override
--- service/src/main/java/dk/kvalitetsit/ihexdsapi/service/impl/Iti41ServiceImpl.java
+++ service/src/main/java/dk/kvalitetsit/ihexdsapi/service/impl/Iti41ServiceImpl.java
@@ -1,21 +1,19 @@
 package dk.kvalitetsit.ihexdsapi.service.impl;
 
 import dk.kvalitetsit.ihexdsapi.dgws.DgwsSoapDecorator;
-import dk.kvalitetsit.ihexdsapi.service.UploadService;
+import dk.kvalitetsit.ihexdsapi.service.Iti41Service;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
 import org.openehealth.ipf.commons.ihe.xds.core.ebxml.EbXMLFactory;
 import org.openehealth.ipf.commons.ihe.xds.core.ebxml.ebxml30.EbXMLFactory30;
 import org.openehealth.ipf.commons.ihe.xds.iti41.Iti41PortType;
-import org.openehealth.ipf.commons.ihe.xds.iti43.Iti43PortType;
 
-public class UploadServiceImpl implements UploadService {
+public class Iti41ServiceImpl implements Iti41Service {
 
     private Iti41PortType iti41PortType;
     private DgwsSoapDecorator dgwsSoapDecorator = new DgwsSoapDecorator();
     private static final EbXMLFactory ebXMLFactory = new EbXMLFactory30();
-
-    public UploadServiceImpl(Iti41PortType iti41PortType) {
+    public Iti41ServiceImpl(Iti41PortType iti41PortType) {
         this.iti41PortType = iti41PortType;
 
         Client proxy = ClientProxy.getClient(iti41PortType);
